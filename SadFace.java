5616efaef90c36adc09f5c59092c54168e92a860
NAKUL DEB NATH <nakuldeb.cse.ru@gmail.com>
2018-01-22 23:19:46 +0600

    Sad Face

---
+++ SadFace.java
@@ -0,0 +1,29 @@
+import java.applet.*;
+import java.awt.Color;
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+public class SadFace extends Applet {
+
+    public SadFace() {
+
+        setBackground(Color.LIGHT_GRAY);
+    }
+
+    public void paint(Graphics w) {
+
+        w.setColor(Color.YELLOW);
+        w.fillOval(250, 150, 350, 320);
+
+        w.setColor(Color.blue);
+        w.fillOval(300, 220, 90, 100);
+        w.fillOval(450, 220, 90, 100);
+
+        w.setColor(Color.BLACK);
+        w.drawArc(345, 380, 150, 80, 0, 180);
+        w.drawLine(420, 260, 420, 360);
+
+        w.setColor(Color.GREEN);
+
+    }
+}
